d9f90e3206955de0a2a7737aeb9a817d5ec8a72e
BebekJK <kevinjk2802@gmail.com>
2025-02-05 14:59:18 +0800

    feat: add color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,14 @@ export default {
         sans: ['Open Sans', 'Arial', 'Helvetica', 'sans-serif'],
       },
       colors: {
+        dip: {
+          20: "#F5F3EF",
+          40: "#E5E0D5",
+          60: "#D1CAB9",
+          80: "#C0AC8F",
+          100: "#877459",
+          blk: "#171717"
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
