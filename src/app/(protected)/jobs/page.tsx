f022ba0c698131636af6c459997d5b74e8ff485e
BebekJK <kevinjk2802@gmail.com>
2025-03-05 18:05:53 +0800

    feat: job scraper pagination

--- src/app/(protected)/jobs/page.tsx
+++ src/app/(protected)/jobs/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 
-import { link } from "fs";
 import JobDisplay from "./(components)/jobdisplay";
 import { useState } from "react";
 import scraper from '@/lib/app/job/api/scrape';
 import { JobScraperResponse } from "@/lib/app/job/types";
+import clsx from "clsx";
 
 const JobsPage: React.FC = () => {
     const [jobs, setJobs] = useState<JobScraperResponse>([]);
     const [search, setSearch] = useState<string>('');
     const [searchDisplay, setSearchDisplay] = useState<string>('');
+    const [pageNumber, setPageNumber] = useState<number>(1);
 
     const handleSubmit = async () => {
         setSearchDisplay(search);
         const data = await scraper.get({
             field: search,
-            page: null
+            page: pageNumber
         })
         setJobs(data);
     }
+
     return (
         <div className="bg-dip-20 w-full min-h-screen py-8">
             <div className='w-[56rem] mx-auto flex justify-between'>
@@ -35,10 +37,17 @@ const JobsPage: React.FC = () => {
                 <div className='w-[36rem] bg-white shadow-xl rounded-xl py-4 px-4'>
                     <div className="flex justify-between">
                         <input type="text" placeholder="Search for jobs" className='w-4/5 bg-dip-20 rounded-full py-2 px-4 mb-4 focus:outline-none' onChange={(e)=>(setSearch(e.target.value))}/>
-                        <button className=" bg-dip-100 rounded-full py-2 px-6 text-white font-bold h-10" onClick={handleSubmit}>Search</button>
+                        <button className=" bg-dip-100 rounded-full py-2 px-6 text-white font-bold h-10" onClick={()=>{setPageNumber(1); handleSubmit()}}>Search</button>
                     </div>
                     <div>{searchDisplay && `Search for: ${searchDisplay}`}</div>
-                    {jobs.map((job, index) => (<JobDisplay key={index} job={job} />))}
+                    {jobs.length? jobs.map((job, index) => (<JobDisplay key={index} job={job} />)) : ""}
+                    {jobs.length?<div className="flex w-36 mx-auto justify-between">
+                        {[1,2,3,4,5].map((page, index) => (
+                            <div key={index} onClick={()=>{if(page!==pageNumber){setPageNumber(page); handleSubmit()}}} className={clsx(page===pageNumber?"bg-slate-200":"", "rounded-full w-8 h-8 flex items-center justify-center")}>
+                                {page}
+                            </div>
+                        ))}
+                    </div> : ""}
                 </div>
             </div>
         </div>
